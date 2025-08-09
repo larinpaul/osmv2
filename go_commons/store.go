8746e55eb52236e8d8cfa802161fca277b3bd898
larinpaul <www.larinpavel@mail.ru>
2025-08-09 11:19:22 +0300

    func (s *store) Create(context.Context) { 	return nil }

--- go_commons/store.go
+++ go_commons/store.go
@@ -1,9 +1,15 @@
 package main
 
+import "context"
+
 type store struct {
 	// add here our mongoDB instance
 }
 
 func NewStore() *store { // constructor for the store
 	return &store{}
+}
+
+func (s *store) Create(context.Context) {
+	return nil
 }

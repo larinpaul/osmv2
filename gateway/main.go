30c34c347ba431ee413efc6acadd48848780a102
larinpaul <www.larinpavel@mail.ru>
2025-08-11 07:51:05 +0300

    func main() { 	mux := http.NewServeMux() 	handler := NewHandler() 	handler.registerRoutes(mux)
    	log.Printf("Starting HTTP server at %s", httpAddr)
    
    	if err := http.ListenAndServe(httpAddr, mux); err != nil {
    		log Fatal("Failed to start http server")
    	}
    }

--- gateway/main.go
+++ gateway/main.go
@@ -15,6 +15,9 @@ func main() {
 	handler := NewHandler()
 	handler.registerRoutes(mux)
 
+	log.Printf("Starting HTTP server at %s", httpAddr)
+
 	if err := http.ListenAndServe(httpAddr, mux); err != nil {
 		log Fatal("Failed to start http server")
 	}
+}

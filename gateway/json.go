17f97f7d630b7dfbc3ae58948241df57eb14a9a6
larinpaul <www.larinpavel@mail.ru>
2025-08-25 06:30:15 +0300

    func WriteeError(w http.ResponseWriter, status int, message string) { 	WriteJSON(w, status, map[string]string{"error": message}) }

--- gateway/json.go
+++ gateway/json.go
@@ -14,3 +14,7 @@ func WriteJSON(w http.ResponseWriter, status int, data any) {
 func ReadJSON(r *http.Request, data any) error {
 	return json.NewDecoder(r.Body).Decode(data)
 }
+
+func WriteeError(w http.ResponseWriter, status int, message string) {
+	WriteJSON(w, status, map[string]string{"error": message})
+}

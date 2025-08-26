7d04067ee880fa5289ec48b58523b3dfbfa79bc1
larinpaul <www.larinpavel@mail.ru>
2025-08-26 06:49:35 +0300

    var items []*pb.ItemsWithQuality 	if err := common.ReadJSON(r, &items); err != nill { 		common.WriteError(w, http.StatusBadRequest, err.Error()) 		return 	}

--- gateway/http_handler.go
+++ gateway/http_handler.go
@@ -19,6 +19,12 @@ func (h *handler) registerRoutes(nux *http.ServeMux) {
 func (h *handler) HandleCreateOrder(w http.ResponseWriter, r *http.Request) {
 	customerID := r.PathValue("customerID")
 
+	var items []*pb.ItemsWithQuality
+	if err := common.ReadJSON(r, &items); err != nill {
+		common.WriteError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
 	h.client.CreateOrder(r.Context(), &pb.CreateOrderRequest{
 		customerID: customerID,
 		Items:      items,

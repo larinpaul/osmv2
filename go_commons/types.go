1fe7252265a9957af457dace37868d0d53c8f5a9
larinpaul <www.larinpavel@mail.ru>
2025-09-20 08:16:58 +0300

    type OrdersService interface { 	CreateOrder(context.Context) error 	ValidateOrder(context.Context, *pb.CreateOrderRequest) error }

--- go_commons/types.go
+++ go_commons/types.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+
+	pb "github.com/sizokonpc/commons/api"
 )
 
 type OrdersService interface {
 	CreateOrder(context.Context) error
-	ValidateOrder(context.Context, pb.) error
+	ValidateOrder(context.Context, *pb.CreateOrderRequest) error
 }
 
 type OrdersStore interface {
